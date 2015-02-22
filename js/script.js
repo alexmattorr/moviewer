8650321436fdfe4f90798cacd3559bf4296ce405
Alex <alexmattorr@gmail.com>
2015-02-21 16:24:45 -0800

    working on functionality

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function baseJS() {
+	$(document).ready(function() {
+		function slideTab() {
+			$(".slide-tab").click(function() {
+				// $('.gallery').addClass("is-active");
+			});
+		};
+
+		slideTab();
+	});
+};
+
+baseJS();
