27c984d80f706bf4cb6f53e102c1841937c28d60
Alex <alexmattorr@gmail.com>
2015-03-18 14:31:12 -0700

    posting to database, now trying to pull data.

---
+++ database.php
@@ -0,0 +1,23 @@
+<?php
+	$host="localhost"; 
+	$username="root";
+	$password="root"; 
+	$db_name="moviewer"; 
+
+	$con=mysql_connect("$host", "$username", "$password");
+
+	if(!$con) {
+		die("Database connection failed: " . mysql_error());
+	}
+
+	mysql_select_db(moviewer);
+	$sql = "INSERT INTO movie_reviews (stars, comment, username) VALUES ('$_POST[stars]', '$_POST[comment]', '$_POST[username]')";
+
+	if(!mysql_query($sql, $con)) {
+		die('Error:' . mysql_error());
+	}
+
+	echo "Comment Recorded";
+
+	mysql_close($con);
+?>
